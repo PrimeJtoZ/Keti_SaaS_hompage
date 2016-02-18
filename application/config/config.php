6610ee4afa024707a269da6e679a5c3d41d39a22
maninblack <qkrwhddn89@gmail.com>
2016-02-18 08:33:56 +0000

    2016.02.18

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+//$config['base_url'] = 'https://keti-saas-hompage-maninbalck.c9users.io';
 $config['base_url'] = '';
+
 
 /*
 |--------------------------------------------------------------------------
